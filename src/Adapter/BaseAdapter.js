20e8648f99c503c3082d47d185ff444c3dd1ca03
bortolig <bortolig@dnb.com>
2018-08-15 16:00:18 -0300

    Trimmed file names with ending slashes

--- src/Adapter/BaseAdapter.js
+++ src/Adapter/BaseAdapter.js
@@ -39,6 +39,11 @@ class BaseAdapter {
             path = path.substring(1);
         }
 
+        let lastPos = path.length - 1;
+        if (path[lastPos] === "/") {
+            path = path.substring(0, lastPos);
+        }
+
         let newPath = [];
         let pathPrefix = this.getPathPrefix();
 
@@ -49,6 +54,25 @@ class BaseAdapter {
         newPath.push(path);
 
         return newPath.join('/');
+    }
+
+    removePathPrefix(path) {
+        let newPath = path.substring(this.getPathPrefix().length);
+
+        if (newPath.length === 0) {
+            return newPath;
+        }
+
+        if (newPath[0] === "/") {
+            newPath = newPath.substring(1);
+        }
+
+        let lastPos = newPath.length - 1;
+        if (newPath[lastPos] === "/") {
+            newPath = newPath.substring(0, lastPos);
+        }
+
+        return newPath;
     }
 }
 
