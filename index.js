27ba60f995de19f64ce9cf7be6cc1d3b985c7e55
bortolig <bortolig@dnb.com>
2018-08-20 09:16:25 -0300

    Upgraded focaccia local adapter

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const BaseAdapter = require("./src/Adapter/BaseAdapter");
 const CONSTANTS = require("./src/constants");
 const Focaccia = require("./src/Focaccia");
+const LocalAdapter = require("./src/Adapter/Local");
 const Utils = require("./src/utils");
 
 module.exports = {
     Focaccia,
     BaseAdapter,
+    LocalAdapter,
     Utils,
     CONSTANTS
 }
